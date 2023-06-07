f06f1ddb826151d9607526251c986c6a21bb8c9d
Nico Kozume <n.struchholtz@gmail.com>
2023-06-07 23:58:10 +0200

    created login tests and removed not needed ones

--- maximago_probearbeit/src/app/services/login.service.spec.ts
+++ maximago_probearbeit/src/app/services/login.service.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 
-import { LoginService } from './login.service';
+import { LoginResult, LoginService } from './login.service';
 
 describe('LoginService', () => {
   let service: LoginService;
@@ -13,4 +13,18 @@ describe('LoginService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should login successfull', () => {
+    expect(service.login("user1@maximago.de", "P@ssw0rd")).toBe(LoginResult.Success)
+  });
+
+  it('should login unsuccessful', () => {
+    expect(service.login("user1@maximago.de", "123")).toBe(LoginResult.Not_Success)
+  });
+
+  it('should logout correctly', () => {
+    service.login("user1@maximago.de", "P@ssw0rd");
+    service.logout()
+    expect(localStorage.getItem("USER")).toBeNull()
+  })
 });
